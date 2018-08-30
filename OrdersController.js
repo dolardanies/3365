e5eb9db3ce0689abe6440ad28de026e424830aca
unknown <2104481@labinfo.is.escuelaing.edu.co>
2018-08-30 17:44:39 -0500

    Inicial

---
+++ OrdersController.js
@@ -0,0 +1,16 @@
+var lista = {
+	 " order_id " :  1 ,
+	 " table_id " :  1 ,
+	 " productos " : [{
+			 " producto " :  " PIZZA " ,
+			 " cantidad " :  3 ,
+			 " precio " :  " $ 15.000 "}
+		,
+		{
+			" producto " :  " HAMBURGUESA " ,
+			 " cantidad " :  1 ,
+			 " precio " :  " $ 12.300 "
+		}
+	]
+}
+
