3b45e03f71f5e5f2cb8f7442ce5eda0e9ce06ac8
d.castiblanco <dolardanies@hotmail.com>
2018-09-12 12:10:24 -0500

    Construccion script parte 3

--- src/main/resources/static/js/OrdersController.js
+++ src/main/resources/static/js/OrdersController.js
@@ -17,9 +17,9 @@ addOrder = function () {
     var insert = {2: {"orderAmountsMap": {"HOTDOG": 10, "HAMBURGUER": 20, "BEER": 40}, "tableNumber": 2}};
     axios.post('/orders', insert)
             .then(function () {
-                $("#actualT").append("<p>Orden 1. </p>");
-                $("#actualT").append("<table class="table"><thead class="thead - dark"><tr><th scope="col">PRODUCTO</th><th scope="col">CANTIDAD</th><th scope="col">PRECIO</th></tr></thead>");
-                        for (map in insert[2].orderAmountsMap) {
+                $("#actualT").append("<p id='tag" + 2 + "'>Order 1</p>");
+                $("#actualT").append("<table id='Order" + 2 + " 'class='table'><thead class='thead - dark'><tr><th scope='col'>PRODUCTO</th><th scope='col'>CANTIDAD</th><th scope='col'>PRECIO</th></tr></thead>");
+                for (map in insert[2].orderAmountsMap) {
                     $("#Order" + 2).append("<tbody> <tr> <td>" + map + "</td> <td>" + insert[2].orderAmountsMap[map] + "</td> </tr> </tbody>");
                 }
 
@@ -49,8 +49,8 @@ loadOrdersList = function () {
                 orders = result.data;
                 $("#actualT").empty();
                 for (key in orders) {
-                    $("#actualT").append("<p id='tag" + key + "'>Order " + key + "</p>");                    
-                    $("#actualT").append("<table id='Order" + key + "' class='table table-dark'> <thead> <tr> <th scope='col'>Product</th> <th scope='col'>Quantity</th> </tr> </thead>");
+                    $("#actualT").append("<p id='tag" + key + "'>Order " + key + "</p>");
+                    $("#actualT").append("<table id='Order" + key + " 'class='table'><thead class='thead - dark'><tr><th scope='col'>PRODUCTO</th><th scope='col'>CANTIDAD</th><th scope='col'>PRECIO</th></tr></thead>");
                     for (map in orders[key].orderAmountsMap) {
                         $("#Order" + key).append("<tbody> <tr> <td>" + map + "</td> <td>" + orders[key].orderAmountsMap[map] + "</td> </tr> </tbody>");
                     }
@@ -61,6 +61,9 @@ loadOrdersList = function () {
                 console.log(error);
                 errorMessage();
             });
+}
+errorMessage = function () {
+    alert("Hay un problema con nuestros servidores. Pedimos disculpas por la inconveniencia, intente de nuevo más tarde");
 }
 
 
