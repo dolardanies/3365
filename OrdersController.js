8c60f7b4e57e7525d55ccd07b1c1da917ec44efb
d.castiblanco <dolardanies@hotmail.com>
2018-09-12 11:27:16 -0500

    Conexion proyecto anterior con este

--- OrdersController.js
+++ OrdersController.js
@@ -1,4 +1,4 @@
-var lista = {
+var orders = {
     " order_id ": 1,
     " table_id ": 1,
     " productos ": [{
@@ -13,36 +13,55 @@ var lista = {
         }
     ]
 }
-addOrder = function(){
-	var insert = {2:{"orderAmountsMap":{"HOTDOG":10,"HAMBURGUER":20,"BEER":40},"tableNumber":2}};
-	axios.post('/orders', insert)
-		.then(function(){                
-			$("#tablasActuales").append("<p>Orden 1. </p>");
-                        $("#tablasActuales").append("<table class="table"><thead class="thead-dark"><tr><th scope="col">PRODUCTO</th><th scope="col">CANTIDAD</th><th scope="col">PRECIO</th></tr></thead>");
-			for(map in insert[2].orderAmountsMap){
-				//Render the rows
-				$("#Order"+2).append("<tbody> <tr> <td>"+map+"</td> <td>"+insert[2].orderAmountsMap[map]+"</td> </tr> </tbody>");
-			}
-			
-		})
-		.catch(function(error){
-			console.log(error);
-			errorMessage();
-		});
+addOrder = function () {
+    var insert = {2: {"orderAmountsMap": {"HOTDOG": 10, "HAMBURGUER": 20, "BEER": 40}, "tableNumber": 2}};
+    axios.post('/orders', insert)
+            .then(function () {
+                $("#actualT").append("<p>Orden 1. </p>");
+                $("#actualT").append("<table class="table"><thead class="thead - dark"><tr><th scope="col">PRODUCTO</th><th scope="col">CANTIDAD</th><th scope="col">PRECIO</th></tr></thead>");
+                        for (map in insert[2].orderAmountsMap) {
+                    $("#Order" + 2).append("<tbody> <tr> <td>" + map + "</td> <td>" + insert[2].orderAmountsMap[map] + "</td> </tr> </tbody>");
+                }
+
+            })
+            .catch(function (error) {
+                console.log(error);
+                errorMessage();
+            });
 }
 
-removeOrderById = function(id){
-	axios.delete('/orders/'+id)
-		.then(function(){
-                        document.getElementById("tag"+id).remove();
-			document.getElementById("Order"+id).remove();
-		})
-		.catch(function(error){
-			console.log(error);
-			errorMessage();
-		});
+removeOrderById = function (id) {
+    axios.delete('/orders/' + id)
+            .then(function () {
+                document.getElementById("tag" + id).remove();
+                document.getElementById("Order" + id).remove();
+            })
+            .catch(function (error) {
+                console.log(error);
+                errorMessage();
+            });
 }
 
+loadOrdersList = function () {
+    orders = [];
+    axios.get('/orders')
+            .then(function (result) {
+                orders = result.data;
+                $("#actualT").empty();
+                for (key in orders) {
+                    $("#actualT").append("<p id='tag" + key + "'>Order " + key + "</p>");                    
+                    $("#actualT").append("<table id='Order" + key + "' class='table table-dark'> <thead> <tr> <th scope='col'>Product</th> <th scope='col'>Quantity</th> </tr> </thead>");
+                    for (map in orders[key].orderAmountsMap) {
+                        $("#Order" + key).append("<tbody> <tr> <td>" + map + "</td> <td>" + orders[key].orderAmountsMap[map] + "</td> </tr> </tbody>");
+                    }
+                }
+                console.log(orders);
+            })
+            .catch(function (error) {
+                console.log(error);
+                errorMessage();
+            });
+}
 
 
 
